bcf1f2da7a7df4e696faf3ed9d54fc6e8295d3db
Sally Lait <sally@recordssoundthesame.com>
2020-12-26 20:42:17 +0000

    Some initial setup and config work

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dir: {
+        // Renaming default (_site)
+        output: "DEPLOY",
+
+        // Specify includes and layouts dir
+        includes: "_includes",
+        layouts: "_layouts"
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Page header
-# emstori.es
+# EM Stories
+A site for me to use as an excuse to learn 11ty, and to tell some stories.
+More info coming soon... (let's build it first)
